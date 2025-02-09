269e6c74550435483671ac766c77936d269952a4
Merrell Ehrmantraut <95222587+merrelle-official@users.noreply.github.com>
2025-02-10 00:51:50 +0300

    added api

--- scripts/script.js
+++ scripts/script.js
@@ -7,6 +7,7 @@ const progressBar = document.querySelector(".progress__bar");
 
 const radius = 120;
 const circumference = 2 * Math.PI * radius;
+
 progressRing.style.strokeDasharray = circumference;
 progressRing.style.strokeDashoffset = circumference;
 
@@ -15,22 +16,35 @@ function toggleClass(element, className, condition) {
 }
 
 input.addEventListener("input", (e) => {
-    const value = Number(e.target.value);
-    if (isNaN(value)){
-        alert("Введите всё-таки число, а не что-то другое, пожалуйста.");
-        return;
-    };
-    
-    const percent = Math.max(0, Math.min(100, e.target.value));
-    const offset = circumference - (percent / 100) * circumference;
-    progressRing.style.strokeDashoffset = offset;
+    progressBarAPI.setValue(e.target.value);
 });
 
 animateInput.addEventListener("change", (e) => {
-    progressOptionsElements.forEach(element => toggleClass(element, "progress__rotate", e.target.checked));
-    toggleClass(progressBar, "progress__rotate", e.target.checked);
+    progressBarAPI.toggleAnimation(e.target.checked);
 });
 
 hideInput.addEventListener("change", (e) => {
-    progressBar.classList.toggle("hidden", e.target.checked);
+    progressBarAPI.toggleVisibility(e.target.checked);
 });
+
+const progressBarAPI = {
+    setValue(value) {
+        value = Math.min(Math.max(value, 0), 100);
+        const offset = circumference - (value / 100) * circumference;
+        progressRing.style.strokeDashoffset = offset;
+        input.value = value;
+    },
+
+    toggleAnimation(isAnimated) {
+        progressOptionsElements.forEach(element => toggleClass(element, "progress__rotate", isAnimated));
+        toggleClass(progressBar, "progress__rotate", isAnimated);
+        animateInput.checked = isAnimated;
+    },
+
+    toggleVisibility(isHidden) {
+        progressBar.classList.toggle("hidden", isHidden);
+        hideInput.checked = isHidden;
+    }
+};
+
+window.progressBarAPI = progressBarAPI;
